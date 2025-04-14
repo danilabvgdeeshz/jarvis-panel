eb27245566b0309b94eed53f7ab930f3809a1bad
Danil <danilabvgdeeshz@gmail.com>
2025-04-14 20:41:09 +0600

    yep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Dashboard from './components/Dashboard';
 import BackgroundGradient from './components/BackgroundGradient';
 
-export default function Page() {
+export default function () {
   const pageStyle: React.CSSProperties = {
     backgroundColor: '#000',
     minHeight: '100vh',
     position: 'relative',
-  };  
+  };
 
   return (
     <main style={pageStyle}>
